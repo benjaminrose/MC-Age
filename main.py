d6a8677a6084b0f1c546b798d50ec4760075da0a
Benjamin Rose <benjamin.rose@me.com>
2017-01-20 10:21:31 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+""" main.py -- Looking for correlations between HR and local environments/ages
+
+Benjamin Rose
+brose3@nd.edu
+benjamin.rose@me.com
+University of Notre Dame
+2017-01-19
+Python 3
+"""
+
+"""
+# Background
+
+"""
+
+"""
+# Goals
+
+"""
