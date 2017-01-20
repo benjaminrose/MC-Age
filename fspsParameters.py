e9d78082888a5036fb9a9ddccf83f9abed217c19
Benjamin Rose <benjamin.rose@me.com>
2017-01-20 11:10:40 -0500

    Test FSPS parameters, specifically `compute_vega_mags`

---
+++ fspsParameters.py
@@ -0,0 +1,41 @@
+""" fspsParameters.py -- A file to help determine the correct FSPS parameters to use.
+
+Benjamin Rose
+brose3@nd.edu
+benjamin.rose@me.com
+University of Notre Dame
+2017-01-20
+Python 3
+"""
+import numpy as np
+import fsps
+
+def test_compute_vega_mags():
+    """
+    Does explicitly turning off `compute_vega_mags` matter in the output of fsps?
+
+    # Parameters
+
+    sp : fsps.StellarPopulation
+        An instance of the FSPS StellarPopulation class. 
+    """
+    filters = ['sdss_u', 'sdss_g', 'sdss_r', 'sdss_i', 'sdss_z']
+    age = 13.7
+
+    #somewhere someone said to only have one instance of 
+    #`fsps.StellarPopulation()` but to test the defaults I need a clean 
+    #instance.
+    sp = fsps.StellarPopulation()
+
+    # test default (should be `False`)
+    mags1 = sp.get_mags(tage=age, bands=filters)
+
+    #test like what many do (set to `False`)
+    #We have to reinitialize, because `compute_vega_mags` can only be changed 
+    #at initiation.
+    sp = fsps.StellarPopulation(compute_vega_mags = False)
+    mags2 = sp.get_mags(tage=age, bands=filters)
+
+    #compare to the milimag (ish)
+    #could use `np.allclose` if I want to drop this down to a single boolean
+    return np.isclose(mags1, mags2)
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ University of Notre Dame
 Python 3
 """
 
+import fsps
+
 """
 # Background
 
@@ -17,3 +19,40 @@ Python 3
 # Goals
 
 """
+
+"""
+# Data Selection
+
+"""
+
+"""
+# FSPS Parameters
+
+FSPS has a large number of parameters, many have clear choices, but few need investigation.
+
+## add_igm_absorption
+
+I am going to leave IGM off.
+
+## zcontinuous
+
+We want to use `logzsol` and  let the metallicity of the sample vary a bit (`pmetals`), so we set `zcontinuous = 2`
+
+## cloudy_dust
+
+`True`, dust matters!
+
+## sfh
+
+Looking at Simha 2014, we select the lin-exp model. This get around the systematically over estimate of age of that basic tau-model. Hoping for more accuracy (and justifiably because we are looking at local environments not galaxies as a whole) we will still let t_i vary. (`sfh = 4`)
+
+## compute_vega_mags
+
+The default is `False`, but the examples explicitly set it. Verified below. And passes!
+
+## add_neb_emission
+How important is this? I think it is very important for hosts with large positive r-i (> 1). 
+"""
+import fspsParameters 
+
+print(fspsParameters.test_compute_vega_mags())
