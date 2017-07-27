3ee9b1f508c2583bcd8a314827ab7c0497526f93
Benjamin Rose <benjamin.rose@me.com>
2017-07-27 16:16:03 -0400

    add a few more tests and fix push to codecov

--- test_calculateAge.py
+++ test_calculateAge.py
@@ -23,3 +23,24 @@ class Test_lnprior():
         redshift = 0.1
         theta = [0, 1, 1, 2, 5, 0, -25]
         assert calculateAge.lnprior(theta, redshift) > -np.inf, "Should this fail?"
+
+class Test_SFH():
+    def test_ramp_lowbounds(self):
+        assert calculateAge.ramp(-0.01) == 0
+        assert calculateAge.ramp(-5) == 0
+        assert calculateAge.ramp(-500) == 0
+
+    def test_ramp_inbounds(self):
+        assert calculateAge.ramp(0) == 0
+        assert calculateAge.ramp(1) == 1
+        assert calculateAge.ramp(100) == 100
+
+    def test_heavyside_lowbounds(self):
+        assert calculateAge.heavyside(-0.01) == 0
+        assert calculateAge.heavyside(-5) == 0
+        assert calculateAge.heavyside(-500) == 0
+
+    def test_heavyside_inbounds(self):
+        assert calculateAge.heavyside(0) == 1
+        assert calculateAge.heavyside(1) == 1
+        assert calculateAge.heavyside(100) == 1
