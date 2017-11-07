benjaminrose/MC-Age

commit bd7e14affa854bec0ac3bc3a6cf68026ade077fe

Correct issue when trying to call 'gupta' or 'messier' tests
Yeah previous changes only half changing the CLI! Also refactored the
code to remove repetition.

fsps-age.py
@@ -156,18 +156,18 @@ def redoGupta(cli):
 
     # todo: how should the CLI switch between Messier and Gupta?
     if cli['gupta']:
-        redoGupta.redoGupta(int(cli['JOBID']), int(cli['JOBLENGTH']),
-                        cli['--debug'], cli['gupta'])
+        dataset = 'gupta'
     elif cli['messier']:
-        redoGupta.redoGupta(int(cli['JOBID']), int(cli['JOBLENGTH']),
-                        cli['--debug'], cli['gupta'])
+        dataset = 'messier'
     elif cli['circle']:
-        redoGupta.redoGupta(int(cli['JOBID']), int(cli['JOBLENGTH']),
-                        cli['--debug'], 'circle')
+        dataset = 'circle'
     else:
         raise ValueError("The test functionality only works if 'gupta',",
                          " 'messier' or 'circle' is called. You somehow got",
                          " passed the CLI and did not select one of these.")
+    
+    redoGupta.redoGupta(int(cli['JOBID']), int(cli['JOBLENGTH']),
+                        cli['--debug'], )
 
 
 def burnin(cli):

commit cc16296408621caa573ae61258bd76380afbf907

Fix typo that did not allow for running of messier or gupta data sets
Bug introduced in bd7e14a

fsps-age.py
@@ -167,7 +167,7 @@ def redoGupta(cli):
                          " passed the CLI and did not select one of these.")
     
     redoGupta.redoGupta(int(cli['JOBID']), int(cli['JOBLENGTH']),
-                        cli['--debug'], )
+                        cli['--debug'], dataset)
 
 
 def burnin(cli):