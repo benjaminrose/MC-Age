24e7eb737f9db2eb48566437bc9a247fb31e6570
Benjamin Rose <brose3@nd.edu>
2018-05-06 14:07:50 -0400

    Add test to check if changing dust changes stellar population output
    May be a duplicate test (with likelihood function) but this is testing `util.sp()` directly and I was worried once so I wrote a test.

--- test_util.py
+++ test_util.py
@@ -16,6 +16,38 @@ class TestSP():
         assert sp.params['add_neb_emission'] == True
         assert sp.params['sfh'] == 5
 
+    # def metals_dust_effects_magnitudes(self):
+
+    def test_dust_effects_magnitudes(self):
+        """
+        Varying the dust parameters, like what the MCMC does, should
+        change the output SED.
+        """
+        #getting a new stellar population is not a great idea for speed.
+        sp = util.get_sp()
+        sdss_bands = ['sdss_u', 'sdss_g', 'sdss_r', 'sdss_i', 'sdss_z']
+
+        # mean of prior
+        sp.params['dust1'] = 0.6
+        sp.params['dust2'] = 0.3
+        one = sp.get_mags(tage=13, bands=sdss_bands)
+
+        # one sigma of prior
+        sp.params['dust1'] = 1.0
+        sp.params['dust2'] = 0.5
+        two = sp.get_mags(tage=13, bands=sdss_bands)
+
+        assert not np.allclose(one, two)
+
+    # def sf_start_dust_effects_magnitudes(self):
+
+    # def sf_tau_dust_effects_magnitudes(self):
+
+    # def sf_transition_dust_effects_magnitudes(self):
+
+    # def sf_slope_dust_effects_magnitudes(self):
+
+
 class TestMode():
     mode = util.mode(np.random.randn(1000))
     def test_return_length(self):
