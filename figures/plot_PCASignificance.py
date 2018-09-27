71d042ed35fe12179466b041a06973e46b6d53de
Benjamin Rose <brose3@nd.edu>
2018-09-27 15:57:52 -0400

    Figure updates

--- figures/plot_PCASignificance.py
+++ figures/plot_PCASignificance.py
@@ -10,6 +10,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.mlab as mlab
 
 RHO = np.genfromtxt('../resources/PCASignificance.csv', delimiter=',')
 
@@ -20,10 +21,16 @@ ax = plt.gca()
 ax.tick_params(axis='both', top='on', right='on', direction='in')
 ax.grid(which='major', axis='both', color='0.90', linestyle='-')
 ax.set_axisbelow(True)
-plt.xlim(0, 0.45)
+plt.xlim(0, 0.6)
 
+#plot distribution and measurement line
 sns.distplot(RHO, kde=False)
 # sns.kdeplot(RHO, cumulative=True)
+plt.axvline(x=0.56, color='r', lw=1.5)
+
+#plot a guassian
+x = np.linspace(0, 0.6, 100)
+plt.plot(x, 2200*mlab.normpdf(x, 0, 0.1), linestyle='--', c='k', alpha=0.65)
 
 plt.xlabel("Spearman's Correlation Coefficient")
 plt.ylabel('Occurrences')
